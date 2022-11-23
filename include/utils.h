101ee1b7ecf1b498db9f1ca1e080386d96e4204b
Harold <gngud21@naver.com>
2022-11-22 22:49:33 -0800

    write to file

--- include/utils.h
+++ include/utils.h
@@ -7,5 +7,9 @@
 
 void create_directory(const char *dir);
 char * append(char * string1, char * string2);
+void write_to_file(FILE *fp, char* data, int counter);
+void write_to_file_tmp(char* filename, char* data, int counter);
+void write_stat(char* filename, char* data, int sentCnt, int recCnt);
+//int get_percent(int sentCnt, int recCnt);
 
 #endif //TCPLOSSYNETWORK_UTIL_H
